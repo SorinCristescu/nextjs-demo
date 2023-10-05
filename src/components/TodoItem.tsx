394b4614f7a89c633842881d4e7a90f263c9a07e
Sorin Cristescu <sorincristescu@Sorins-MacBook-Pro.local>
2023-10-05 23:18:20 +0300

    add todo app

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Trash2 } from "lucide-react";
+
+type TodoItemProps = {
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+  deleteTodo: (id: string) => void;
+};
+
+export function TodoItem({ id, title, complete, toggleTodo, deleteTodo }: TodoItemProps) {
+    const handleDelete = () => {
+        deleteTodo(id)
+    }
+  return (
+    <li className="w-full flex items-center justify-between p-4 border rounded-md border-slate-700">
+      <div className="flex gap-1  items-center ">
+        <input
+          id={id}
+          type="checkbox"
+          className="cursor-pointer peer"
+          defaultChecked={complete}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <label
+          htmlFor={id}
+          className="ml-1 cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+        >
+          {title}
+        </label>
+      </div>
+      <div className="flex items-center">
+      <p className="mr-2 text-xs text-slate-500">{complete ? "completed" : ""}</p>
+      <button onClick={handleDelete} className="bg-transparent rounded-md px-2 py-2 outline-none hover:bg-slate-700">
+        <Trash2 className="text-slate-300 " size={14} />
+      </button>
+      </div>
+ 
+    </li>
+  );
+}
